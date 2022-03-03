0c8366f91335efa02c285fe3cebd215f859a6f4c
Ellis Ballard <eab4@njit.edu>
2022-03-03 16:13:26 -0500

    starting calculator_calculations branch

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,26 +1,30 @@
 """ This is the Calculator Class"""
 from calculator.operations import Addition, Subtraction, Multiplication
+"""we want to have a history of calculations, 3/3/22"""
 
 
 class Calculator:
     """ This is the default result property"""
     result = 0
 
-    def add(self, value_1, value_2):
+    """when the method becomes a static method, we no longer need self"""
+    @staticmethod
+    def add(value_1, value_2):
         """ This is the add method"""
-        self.result = Addition.add(value_1, value_2)
-        return self.result
+        # Call the static method add to return the sum and set it to the calculator result
+        return Addition.add(value_1, value_2)
 
-    def subtract(self, value_1, value_2):
+    @staticmethod
+    def subtract(value_1, value_2):
         """ This is the subtract method"""
-        self.result = Subtraction.subtract(value_1, value_2)
-        return self.result
+        return Subtraction.subtract(value_1, value_2)
 
-    def multiply(self, value_1, value_2):
+    @staticmethod
+    def multiply(value_1, value_2):
         """ This is the subtract method"""
-        self.result = Multiplication.multiply(value_1, value_2)
-        return self.result
+        return Multiplication.multiply(value_1, value_2)
 
+    @staticmethod
     def get_result(self):
         """ This is the get result method"""
         return self.result
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,41 +2,17 @@
 from calculator import Calculator
 
 
-def test_calculator_is_instance():
-    """Testing the Calculator"""
-    calculator = Calculator()
-    assert isinstance(calculator, Calculator)
-
-
-def test_calculator_get_result_method():
-    """Testing the Calculator"""
-    calculator = Calculator()
-    assert calculator.get_result() == 0
-
-
-def test_calculator_result_property():
-    """Testing the Calculator"""
-    calc1 = Calculator()
-    calc2 = Calculator()
-    calc1.result = 5
-    calc2.result = 6
-    assert calc1.result == 5
-    assert calc2.result == 6
-
-
 def test_calculator_add_method():
     """Testing the Calculator Addition"""
-    calculator = Calculator()
-    assert calculator.add(1, 1) == 2
+    # Since the methods are now static, no longer need calculator = Calculator since we dont have to instantiate them multiple times
+    assert Calculator.add(1, 1) == 2
 
 
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
-    calculator = Calculator()
-    assert calculator.subtract(1, 1) == 0
+    assert Calculator.subtract(1, 1) == 0
 
 
 def test_calculator_multiply_method():
     """Testing the Calculator Multiply"""
-    calculator = Calculator()
-    assert calculator.multiply(1, 1) == 1
+    assert Calculator.multiply(1, 1) == 1
